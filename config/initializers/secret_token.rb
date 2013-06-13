7ceb7b6b1a23f04bad185810a3c3b94aa20cfe9a
Matthew Hanson <mhanson@limitless-financial.com>
2013-06-12 21:46:42 -0700

    installed key gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Draftful::Application.config.secret_token = '392d3f6825850fabc4b689bf316783356f99487e176a2f8003ee3933889a1ecaa2066c6a80310191cc3df2e71d47b0ae377d20de4f70460a8a02f3a7e6062f09'
